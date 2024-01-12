f6109c24bf50138a6033c76d3e47cfa6f8dc42d4
Damian Tomczyszyn <damitom@o2.pl>
2024-01-12 20:51:03 +0100

    weather api, time server formatted

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,20 @@
+#include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
-#include <StreamString.h>
+//#include <StreamString.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 #include <ArduinoJson.h>
-#include <HTTPClient.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClientSecure.h>
 #include "decisionTree.h"
 
 //---------------------------------------
 
 #ifndef STASSID
-#define STASSID "dlink" // wifi name
-#define STAPSK "tomczyszyn"// wifi password
+#define STASSID "" // wifi name
+#define STAPSK ""// wifi password
 #endif
 #define ZAW1 D0 //zawory
 #define ZAW2 D1 //zawory
@@ -25,10 +26,11 @@
 const char *ssid = STASSID;
 const char *password = STAPSK;
 //---------------------------------------
-ESP8266WebServer server(80); //serwwer do przyjmowania requestów
+ESP8266WebServer server(80); //serwer do przyjmowania requestów http
 String dateAndTime=" ";//date and time init
 
 WiFiClient client;
+
 int program=0;//aktualny program podlewania(0-nie wybrany)
 unsigned long  lastWatheringTime = millis(); // sprawdz czy tyle milis się zmieści ile trzeba - czas od ostatniego podlewania
 bool watheringIsOn=false; // czy podlewanie jest aktualnie właczone
@@ -47,22 +49,34 @@ unsigned long czasCzekania;
 //declare functions
 String getTime();
 void changeWathering();
-//
-
-//weather api
-//https://randomnerdtutorials.com/esp8266-weather-forecaster/
-
-
-
 
 
 unsigned long lastConnectionTime = 0;     // last time you connected to the server, in milliseconds
 const unsigned long postInterval = 10 * 1000;  // posting interval of 10 minutes  (10L * 1000L; 10 seconds delay for testing)
 
+//web api section
+String getHtmlContent(const char* url);
+void getDataFromStation();
+//
+const size_t capacity = JSON_OBJECT_SIZE(900) + 300;
+const char*  serverApi = "weather.visualcrossing.com";  // Server URL
+DynamicJsonDocument doc(capacity);
+WiFiClientSecure clientS;
+String jsonResponse = "";
+const char* jsonString = R"({
+    "Temperature": 25.26,
+    "Pressure": 1000.50,
+    "Altitude": 106.70,
+    "Humidity": 28.79    
+})";
 
- 
+DynamicJsonDocument doc2(200);  
+DeserializationError error = deserializeJson(doc2, jsonString);
 
 
+
+
+void printDayInfo(JsonObject day, int dayNumber);//deklaracja
 
 
 
@@ -83,10 +97,13 @@ void handleRoot() {
 <button id=\"on\">Włącz</button>\
 <button id=\"off\">Wyłącz</button><br>\
 <button id=\"p1\">program1</button><br>\
-<button id=\"p2\">program2</button>");
+<button id=\"p2\">program2</button><br>");
 
 temp.println("Time:");
 temp.println(getTime());
+temp.println("<br>Date:");
+temp.println(dateAndTime);
+temp.println("<br>");
 temp.printf("\
 <script>\
     document.getElementById(\"on\").onclick = function () {const zapytanie = new XMLHttpRequest();zapytanie.open(\"GET\", \"/on\");zapytanie.send();};\
@@ -120,8 +137,63 @@ void handleNotFound() {
   digitalWrite(LED_BUILTIN, 0);
 }
 
+void connectAndSave() {
+  Serial.println("\nStarting connection to server...");
+  clientS.setInsecure();//skip verification
+  if (!clientS.connect(serverApi, 443))
+    Serial.println("Connection failed!");
+  else {
+    Serial.println("Connected to server!");
+    // Make a HTTP request:
+    clientS.println("GET https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Lublin/2024-01-10/2024-01-14?unitGroup=metric&elements=datetime%2Ctempmax%2Ctempmin%2Ctemp%2Cdew%2Chumidity%2Cprecip%2Cprecipprob%2Cpreciptype%2Cwindspeed%2Cwindspeedmax%2Cwindspeedmean%2Cwindspeedmin%2Cpressure%2Ccloudcover%2Cuvindex%2Csunrise%2Csunset&include=fcst%2Cremote%2Cobs%2Cdays&key=&contentType=json HTTP/1.0");
+    clientS.println("Host: weather.visualcrossing.com");
+    clientS.println("Connection: close");
+    clientS.println();
 
-void setup(void) {
+    while (clientS.connected()) {
+      String line = clientS.readStringUntil('\n');
+      
+      Serial.println(line); // HTTP/1.1 200
+    }
+    // if there are incoming bytes available
+    // from the server, read them and print them:
+
+    while (clientS.available()) {
+      char c = clientS.read();
+      Serial.write(c);
+      if(c=='{')
+      {
+        jsonResponse = "{";
+        while (clientS.available())
+        {
+                char c = clientS.read();
+                Serial.write(c);
+          jsonResponse+=c;
+        }
+      }
+    }
+    Serial.println();
+    clientS.stop();
+
+
+
+  //to json
+    deserializeJson(doc, jsonResponse);
+      //Serial.println("Estimated memory usage: " + String(measureJson(doc)) + " bytes");
+    if (doc.isNull()) {
+        Serial.println("Parsing input failed!");
+        return;
+      }
+    
+    Serial.println("JSON object = ");
+    serializeJsonPretty(doc, Serial);
+    Serial.println("Estimated memory usage: " + String(measureJson(doc)) + " bytes");
+  }
+
+}
+
+void setup(void) 
+{
   pinMode(LED_BUILTIN, OUTPUT);//ustawienie leda do świecenia jako output
   pinMode(ZAW1, OUTPUT);// wyjście dla zaworów
   pinMode(ZAW2, OUTPUT);// wyjście dla zaworów
@@ -154,13 +226,20 @@ void setup(void) {
 
 
 
-
   if (MDNS.begin("esp8266")) { 
     Serial.println("MDNS responder started"); 
     }
 
-  server.on("/", handleRoot);
+  connectAndSave();
+  for (int x = 0; x < 5; x++) {
+    // Dostęp do elementów w tablicy "days"
+    JsonObject days = doc["days"][x];
+    printDayInfo(days, x + 1);
+  }
 
+
+
+  server.on("/", handleRoot);
   server.on("/on", [](){
         digitalWrite(LED_BUILTIN, LOW);                                      //zapal diodę
         digitalWrite(ZAW1, HIGH);   // pin13
@@ -171,15 +250,12 @@ void setup(void) {
       digitalWrite(ZAW1, LOW); 
       server.send(200);   
   });
-
-
   server.on("/codziennie", [](){
         
       program=1;
         server.send(200);   });
   
   server.on("/co2dni", []() {
-
       program=2;
       server.send(200);   
   });
@@ -192,11 +268,11 @@ void setup(void) {
 
   getTime();// zapisz czas
   //wylicz ile trzeba poczekać do godziny 21 (zobacz czy sie spieszy godzine czy późni)
-  setupTime=czas[0]*60*60*1000+czas[1]*60*1000+czas[2]*1000;// w ms
+  setupTime = czas[0] *60*60*1000 + czas[1] *60*1000 + czas[2] *1000;// w ms
   //godzina 22 w ms to -> 79 200 000
   if(setupTime>watheringStartupTime)// jest już po czasie
   {
-    czasCzekania= 86400000 - (setupTime-watheringStartupTime)+millis(); // odjąć czas nadwyżki od 24h w ms
+    czasCzekania = 86400000 - (setupTime-watheringStartupTime)+millis(); // odjąć czas nadwyżki od 24h w ms
   }
   else//jest przed czasem
   {
@@ -218,8 +294,6 @@ void setup(void) {
 unsigned long oczekiwanie=0;
 void loop(void) {
 
-  
-  
   server.handleClient();
   MDNS.update();
   delay(500);//czas na odpowiedź
@@ -236,15 +310,13 @@ void loop(void) {
   changeWathering();
 }
 
-
 }
-
 
 
 String getTime(){
 const long utcOffsetInSeconds = 3600;
 
-char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+String daysOfTheWeek[7] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
 // Define NTP Client to get time
 WiFiUDP ntpUDP;
@@ -257,27 +329,30 @@ NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", utcOffsetInSeconds);
   timeClient.begin();
   timeClient.update();
 
-  // Serial.print(daysOfTheWeek[timeClient.getDay()]);
-  // Serial.print(", ");
-  // Serial.print(timeClient.getHours());
-  // Serial.print(":");
-  // Serial.print(timeClient.getMinutes());
-  // Serial.print(":");
-  // Serial.println(timeClient.getSeconds());
+
 
   //Serial.println(timeClient.getFormattedTime());
-  dateAndTime=daysOfTheWeek[timeClient.getDay()]+timeClient.getHours()+timeClient.getMinutes()+timeClient.getSeconds();
+  Serial.println(timeClient.getEpochTime());
+  Serial.println(timeClient.getDay());
+  
+  // Konwersja epoch time na strukturę czasu
+  struct tm *timeinfo;
+  time_t epoch_time = timeClient.getEpochTime();
+  timeinfo = localtime(&epoch_time);
+  // Uzyskanie formatowanej daty i czasu
+  char buffer[20];
+  strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", timeinfo);// mogę sobie uciąć samą datę bez godziny do dziennego API
+
+  // Wyświetlenie sformatowanej daty i czasu
+  Serial.println(buffer);
+  dateAndTime = buffer;
+
   czas[0]=timeClient.getHours();
   czas[1]=timeClient.getMinutes();
   czas[2]=timeClient.getSeconds();
   return (String)timeClient.getFormattedTime();
 
 }
-
-
-
-
-
 
 
 void changeWathering(){
@@ -302,4 +377,46 @@ void changeWathering(){
 
   }
   
+}
+
+void printDayInfo(JsonObject day, int dayNumber) {
+  const char* datetime = day["datetime"];
+  double tempmax = day["tempmax"];
+  double tempmin = day["tempmin"];
+  double temp = day["temp"];
+  double dew = day["dew"];
+  double humidity = day["humidity"];
+  double precip = day["precip"];
+  double precipprob = day["precipprob"];
+  const char* preciptype = day["preciptype"]; // Ostrzeżenie: To pole może być null
+  double windspeed = day["windspeed"];
+  double pressure = day["pressure"];
+  double cloudcover = day["cloudcover"];
+  double uvindex = day["uvindex"];
+  double windspeedmax = day["windspeedmax"];
+  double windspeedmean = day["windspeedmean"];
+  double windspeedmin = day["windspeedmin"];
+  const char* sunrise = day["sunrise"];
+  const char* sunset = day["sunset"];
+
+  Serial.println("Day " + String(dayNumber) + " Info:");
+  Serial.println("Date Time: " + String(datetime));
+  Serial.println("Temp Max: " + String(tempmax));
+  Serial.println("Temp Min: " + String(tempmin));
+  Serial.println("Temp: " + String(temp));
+  Serial.println("Dew: " + String(dew));
+  Serial.println("Humidity: " + String(humidity));
+  Serial.println("Precip: " + String(precip));
+  Serial.println("Precip Prob: " + String(precipprob));
+  Serial.println("Precip Type: " + String(preciptype)); // Może być null
+  Serial.println("Wind Speed: " + String(windspeed));
+  Serial.println("Pressure: " + String(pressure));
+  Serial.println("Cloud Cover: " + String(cloudcover));
+  Serial.println("UV Index: " + String(uvindex));
+  Serial.println("Wind Speed Max: " + String(windspeedmax));
+  Serial.println("Wind Speed Mean: " + String(windspeedmean));
+  Serial.println("Wind Speed Min: " + String(windspeedmin));
+  Serial.println("Sunrise: " + String(sunrise));
+  Serial.println("Sunset: " + String(sunset));
+  Serial.println();
 }
