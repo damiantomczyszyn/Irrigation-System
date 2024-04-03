a6fa6802f03f0b42b2ec84d8b8d409085c9cf89e
Damian Tomczyszyn <damitom@o2.pl>
2024-04-03 21:22:16 +0200

    Ilość 1: 106, co stanowi 70.67% skutecznosci dla tresholdu wilgotnosci gleby 0.25

--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,7 @@
 short dniBezPodlewania = 0;
 float srStopnie = 20;
 float szansaNaOpad = 50;
-float waterLewel = 0.3;
+float waterLewel = 0.25;
 float opadZDwóchDni = 10;
 short iloscDniBezPodlewania = 4;
  /////------------
