1777894204c051e1bd44fae5a1176a40202cf2e2
Damian Tomczyszyn <damitom@o2.pl>
2024-04-04 08:57:36 +0200

    drzewo 5

--- src/decisionTree.cpp
+++ src/decisionTree.cpp
@@ -117,7 +117,7 @@ bool makeWatheringDecision(DynamicJsonDocument doc, DynamicJsonDocument doc2)//(
     //-------------------------
     if(czyJestMokro())
     {// tak jest mokro
-        probabilitySum += -40;
+        probabilitySum += -20;
 
         if (czyPadaloDuzoPrzezOstatnieDwaDni())
         {//tak padalo wiecej niz x litrow
