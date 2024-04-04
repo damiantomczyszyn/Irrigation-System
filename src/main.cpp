817cd314a15e8cee1b4f9a3584168d48fecd376e
Damian Tomczyszyn <damitom@o2.pl>
2024-04-04 13:31:08 +0200

    poprawione testy i dane i funkcja oceny - drzewo01

--- src/main.cpp
+++ src/main.cpp
@@ -26,9 +26,8 @@
 short dniBezPodlewania = 0;
 float srStopnie = 20;
 float szansaNaOpad = 60;
-float szansaNaOpad = 60;
 float waterLewel = 0.3;
-float opadZDwóchDni = 15;
+float opadZDwóchDni = 20;
 short iloscDniBezPodlewania = 3;
  /////------------
 
