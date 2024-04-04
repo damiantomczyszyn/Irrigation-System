002bd620041351e19f91dfd7470fd5e0369f787d
Damian Tomczyszyn <damitom@o2.pl>
2024-04-04 08:27:15 +0200

    ostatni najlepszy 59%

--- src/main.cpp
+++ src/main.cpp
@@ -25,10 +25,10 @@
 //Zmienne alg---------
 short dniBezPodlewania = 0;
 float srStopnie = 20;
-float szansaNaOpad = 50;
-float waterLewel = 0.3;
+float szansaNaOpad = 60;
+float waterLewel = 0.28;
 float opadZDwóchDni = 10;
-short iloscDniBezPodlewania = 4;
+short iloscDniBezPodlewania = 7;
  /////------------
 
 const char *ssid = STASSID;
